9e89d2d543e2e31572bb2e37e786a6849e44e2b1
hossam <hossamg16@gmail.com>
2023-12-06 02:49:23 +0200

    handle nav-bar z-index and adding section favBook

--- client-side/src/components/Home.jsx
+++ client-side/src/components/Home.jsx
@@ -2,11 +2,13 @@
 import React from 'react'
 import Banner from './Banner'
 import FavoriteBooks from '../Pages/Shop/Home/FavoriteBooks'
+import FavBook from '../Pages/Shop/Home/FavBook'
 
 const Home = () => {
   return (
    <><Banner />
    <FavoriteBooks />
+   <FavBook />
    </>
 
   )
